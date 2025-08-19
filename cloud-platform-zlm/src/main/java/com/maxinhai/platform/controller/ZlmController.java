19fb3bb2a42e6524412f530747625975f726c390
maxin <maxinhai@coalbot.cn>
2025-08-19 22:55:01 +0800

    提交zlm模块修改；

--- cloud-platform-zlm/src/main/java/com/maxinhai/platform/controller/ZlmController.java
+++ cloud-platform-zlm/src/main/java/com/maxinhai/platform/controller/ZlmController.java
@@ -1,14 +1,18 @@
 package com.maxinhai.platform.controller;
 
+import com.alibaba.fastjson2.JSONObject;
+import com.maxinhai.platform.dto.AddStreamProxyDTO;
+import com.maxinhai.platform.dto.DelStreamProxyDTO;
+import com.maxinhai.platform.dto.ServiceConfigDTO;
 import com.maxinhai.platform.service.ZlmClient;
 import com.maxinhai.platform.utils.AjaxResult;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import java.util.Map;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/zlm")
@@ -16,17 +20,71 @@ public class ZlmController {
 
     @Resource
     private ZlmClient zlmClient;
-    @Value("${secret:re9VTQe70OiWZ3NrY4L6Eolpy3hlGOoh}")
-    private String secret;
+
+    private static ServiceConfigDTO serviceConfig = null;
+    static {
+        if(Objects.isNull(serviceConfig)){
+            serviceConfig = new ServiceConfigDTO("127.0.0.1", 8080, "EQPSv4wFVeS7e7hJ9YyC3s1Zbs8Njn78");
+        }
+    }
 
     @GetMapping("/getApiList")
-    public AjaxResult<Map<String, Object>> getApiList() {
-        return AjaxResult.success(zlmClient.getApiList(secret));
+    public AjaxResult<JSONObject> getApiList() {
+        return AjaxResult.success(zlmClient.getApiList(serviceConfig));
     }
 
     @GetMapping("/getVersion")
-    public AjaxResult<Map<String, Object>> getVersion() {
-        return AjaxResult.success(zlmClient.getVersion(secret));
+    public AjaxResult<JSONObject> getVersion() {
+        return AjaxResult.success(zlmClient.version(serviceConfig));
+    }
+
+    @GetMapping("/getThreadsLoad")
+    public AjaxResult<JSONObject> getThreadsLoad() {
+        return AjaxResult.success(zlmClient.getThreadsLoad(serviceConfig));
+    }
+
+    @GetMapping("/getWorkThreadsLoad")
+    public AjaxResult<JSONObject> getWorkThreadsLoad() {
+        return AjaxResult.success(zlmClient.getWorkThreadsLoad(serviceConfig));
+    }
+
+    @GetMapping("/getStatistic")
+    public AjaxResult<JSONObject> getStatistic() {
+        return AjaxResult.success(zlmClient.getStatistic(serviceConfig));
+    }
+
+    @GetMapping("/getServerConfig")
+    public AjaxResult<JSONObject> getServerConfig() {
+        return AjaxResult.success(zlmClient.getServerConfig(serviceConfig));
+    }
+
+    @GetMapping("/getMediaList")
+    public AjaxResult<JSONObject> getMediaList() {
+        return AjaxResult.success(zlmClient.getMediaList(serviceConfig));
+    }
+
+    @GetMapping("/getAllSession")
+    public AjaxResult<JSONObject> getAllSession() {
+        return AjaxResult.success(zlmClient.getAllSession(serviceConfig));
+    }
+
+    @GetMapping("/restartServer")
+    public AjaxResult<JSONObject> restartServer() {
+        return AjaxResult.success(zlmClient.restartServer(serviceConfig));
+    }
+
+    @GetMapping("/addStreamProxy")
+    public AjaxResult<JSONObject> addStreamProxy() {
+        AddStreamProxyDTO param = new AddStreamProxyDTO("_defaultVhost_", "demo", "01", "rtsp://127.0.0.1:8554/rtsp");
+        AddStreamProxyDTO.setConfig(param, serviceConfig);
+        return AjaxResult.success(zlmClient.addStreamProxy(param));
+    }
+
+    @GetMapping("/delStreamProxy")
+    public AjaxResult<JSONObject> delStreamProxy(@PathVariable("key") String key) {
+        DelStreamProxyDTO param = new DelStreamProxyDTO(key);
+        DelStreamProxyDTO.setConfig(param, serviceConfig);
+        return AjaxResult.success(zlmClient.delStreamProxy(param));
     }
 
 }
