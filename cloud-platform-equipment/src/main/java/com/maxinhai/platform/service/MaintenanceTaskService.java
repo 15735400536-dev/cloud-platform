052089a9ac27fd577cd060c7e7cdcead85497eb7
maxin <maxinhai@coalbot.cn>
2025-08-28 16:10:16 +0800

    完善设备管理模块；

--- cloud-platform-equipment/src/main/java/com/maxinhai/platform/service/MaintenanceTaskService.java
+++ cloud-platform-equipment/src/main/java/com/maxinhai/platform/service/MaintenanceTaskService.java
@@ -1,7 +1,15 @@
 package com.maxinhai.platform.service;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.maxinhai.platform.dto.MaintenanceTaskQueryDTO;
 import com.maxinhai.platform.po.MaintenanceTask;
+import com.maxinhai.platform.vo.MaintenanceTaskVO;
 
 public interface MaintenanceTaskService extends IService<MaintenanceTask> {
+
+    Page<MaintenanceTaskVO> searchByPage(MaintenanceTaskQueryDTO param);
+
+    MaintenanceTaskVO getInfo(String id);
+
 }
