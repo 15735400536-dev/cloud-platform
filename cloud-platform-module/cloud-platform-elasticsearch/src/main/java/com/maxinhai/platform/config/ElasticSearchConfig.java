1092e3bf29242e41a5d346a9e5aaebb773773c02
maxin <maxinhai@coalbot.cn>
2025-09-03 14:11:08 +0800

    完善ElasticSearch配置类；

--- cloud-platform-module/cloud-platform-elasticsearch/src/main/java/com/maxinhai/platform/config/ElasticSearchConfig.java
+++ cloud-platform-module/cloud-platform-elasticsearch/src/main/java/com/maxinhai/platform/config/ElasticSearchConfig.java
@@ -1,18 +1,23 @@
 package com.maxinhai.platform.config;
 
 import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 /**
  * @ClassName：ElasticsearchConfig
  * @Author: XinHai.Ma
  * @Date: 2025/9/3 11:32
- * @Description: 必须描述类做什么事情, 实现什么功能
+ * @Description: ElasticSearch配置类
  */
 @Configuration
-@EnableElasticsearchRepositories(basePackages = "com.example.esdemo.repository")
-public class ElasticsearchConfig {
+@EnableElasticsearchRepositories(basePackages = "com.maxinhai.platform.repository")
+public class ElasticSearchConfig {
 
     @Bean
     public RestHighLevelClient client() {
