31fe54d0a9312c78e6e2fd50209a80e1f62c9cf9
maxin <maxinhai@coalbot.cn>
2025-09-20 17:12:11 +0800

    重写toString方法，增加根据布尔表达式构建统一返回结果方法；

--- cloud-platform-module/cloud-platform-core/src/main/java/com/maxinhai/platform/utils/AjaxResult.java
+++ cloud-platform-module/cloud-platform-core/src/main/java/com/maxinhai/platform/utils/AjaxResult.java
@@ -3,8 +3,12 @@ package com.maxinhai.platform.utils;
 import lombok.Data;
 import org.springframework.http.HttpStatus;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 统一返回结果
+ *
  * @param <T>
  */
 @Data
@@ -63,4 +67,33 @@ public class AjaxResult<T> {
         return new AjaxResult(HttpStatus.INTERNAL_SERVER_ERROR.value(), HttpStatus.OK.getReasonPhrase(), null);
     }
 
+    public static <T> AjaxResult<T> build(boolean flag, String msg, T data) {
+        return new AjaxResult(flag ? HttpStatus.OK.value() : HttpStatus.INTERNAL_SERVER_ERROR.value(), msg, data);
+    }
+
+    public static <T> AjaxResult<T> build(boolean flag, String msg) {
+        return new AjaxResult(flag ? HttpStatus.OK.value() : HttpStatus.INTERNAL_SERVER_ERROR.value(), msg, null);
+    }
+
+    /**
+     * 重写toString方法，过滤值为null的字段
+     */
+    @Override
+    public String toString() {
+        List<String> fieldList = new ArrayList<>();
+
+        // 只添加非null的字段
+        fieldList.add("code=" + code);  // code为int，不可能为null
+
+        if (msg != null) {
+            fieldList.add("msg=" + msg);
+        }
+
+        if (data != null) {
+            fieldList.add("data=" + data);
+        }
+
+        // 拼接成 "AjaxResult(字段1, 字段2, ...)" 格式
+        return "AjaxResult(" + String.join(", ", fieldList) + ")";
+    }
 }
