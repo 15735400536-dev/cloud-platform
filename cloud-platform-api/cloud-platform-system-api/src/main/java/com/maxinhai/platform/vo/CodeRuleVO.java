00a0c10934157c063233c369bc61059adcf3c2d9
maxin <maxinhai@coalbot.cn>
2026-04-13 01:29:14 +0800

    update：完善规则编码分页查询接口VO；

--- cloud-platform-api/cloud-platform-system-api/src/main/java/com/maxinhai/platform/vo/CodeRuleVO.java
+++ cloud-platform-api/cloud-platform-system-api/src/main/java/com/maxinhai/platform/vo/CodeRuleVO.java
@@ -67,4 +67,11 @@ public class CodeRuleVO {
     @ApiModelProperty(value = "状态（1:启用，0:禁用）")
     private Status status;
 
+    @ApiModelProperty(value = "创建时间")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    protected Date createTime;
+    @ApiModelProperty(value = "修改时间")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    protected Date updateTime;
+
 }
